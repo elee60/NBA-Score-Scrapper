2546bb78fa1b6b265d6b19ef0f5585665b702fd5
kkhurana96 <57566518+kkhurana96@users.noreply.github.com>
2019-11-16 08:56:08 -0500

    Add files via upload

---
+++ Comparator.java
@@ -0,0 +1,8 @@
+public abstract class Comparator{
+  
+  public Comparator(){}
+  
+  public abstract String compare(String a, String b);
+  
+  public abstract String compare(String a, String b, String c);
+}
